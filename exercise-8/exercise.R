3b552b83830cb1d76da8c09fdaacf17d281f6e21
Cindy <chiuyt19@uw.edu>
2017-04-19 17:48:54 -0700

    exercise 8

--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -11,18 +11,19 @@ library(dplyr)
 
 # View in the data set. Start to understand what the data columns contains
 # Be sure to comment out the function so it won't view everytime you run the code.
-
+#View(pulitzer)
 
 # Use 'colnames' to print out the names of the columns
-
+colnames(pulitzer)
 
 # Use 'str' to print what types of values are contained in each column
 # Did any value type surprise you? Why do you think they are that type?
+str(pulitzer)
 
 
 # Add a column in a dataframe called 'Pulitzer.Prize.Change` that contains the diffrence in changes
 # in Pulitzer Prize Winners from 2004 to 2013 and Pultizer Prize Winners from 1990 to 2003.
-
+Pulitzer.Prize.Change
 
 # What publication gained the most pulitzer prizes from 2004-2014?
 # Be sure to use the pipe operator! 
